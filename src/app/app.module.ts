a6f84c09834a87e67818e0723bd4442454215960
Alexey Kolokolov <a.kolokolov@spd-ukraine.com>
2018-01-12 19:17:31 +0200

    Add routind for heroes route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import {HeroService} from './hero.service';
+import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -20,7 +22,8 @@ import { MessageService } from './message.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
